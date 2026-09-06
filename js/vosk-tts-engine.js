e4e7e1520ad078b6334ae497256203beed274b6a
FedorMilovanov <258225528+FedorMilovanov@users.noreply.github.com>
2026-09-06 23:13:04 +0300

    fix(tts): align engine with retired-worker revision

--- js/vosk-tts-engine.js
+++ js/vosk-tts-engine.js
@@ -1,7 +1,7 @@
 (function () {
 'use strict';
 var VERSION = 2;
-var WORKER_SRC = '/js/vosk-tts-worker.js?v=2ea9ada3';
+var WORKER_SRC = '/js/vosk-tts-worker.js?v=7f50541a';
 var NOTICE_CSS_URL = '/css/tts-download-notice.css?v=b9ef192f';
 var MODEL_DOWNLOAD_OPTOUT_KEY = 'gbx-vosk-warmup';
 var CLIENT_ID = (window.crypto && typeof window.crypto.randomUUID === 'function')
