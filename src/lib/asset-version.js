44ac8433448abb42e54b8067868ecda1ded9bf5a
FedorMilovanov <258225528+FedorMilovanov@users.noreply.github.com>
2026-09-16 00:49:54 +0300

    fix(reader): remove redundant Gill important overrides

--- src/lib/asset-version.js
+++ src/lib/asset-version.js
@@ -13,7 +13,7 @@ export const ASSET_VERSIONS = {
   'css/reader-preferences.css': 'b740c21f',
   'css/series-manuscript.css': 'fc3a55ce',
   'css/series-samizdat.css': '2c4a9f29',
-  'css/site.css': '019a8462',
+  'css/site.css': '4a4aa6e8',
   'css/sw-toast.css': '2e540077',
   'css/tts-download-notice.css': 'b9ef192f',
   'fonts/fonts.css': '864cc57a',
